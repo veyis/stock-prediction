28ff91d5549dc58c52e7060c8b7741ff238a453b
veyis <vakgul@gmail.com>
2023-10-03 14:31:43 -0700

    6

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import db_manager as dbm
+import plotly.graph_objects as go
 
   
 def streamlit_settings():
@@ -12,15 +13,69 @@ def streamlit_settings():
     </style>
     """, unsafe_allow_html=True)
 
-    
+    # Custom CSS to hide the hamburger menu and footer
+    hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        </style>
+        """
+    st.markdown(hide_menu_style, unsafe_allow_html=True)
 
+def plot_stock_prices(symbol):
+    # Using your db_manager to get data as a DataFrame
+    db = dbm.Database()
+    df = dbm.get_stock_data_as_dataframe(db, symbol)
+    db.close()
+    
+    # Create a Plotly figure
+    fig = go.Figure()
+
+    # Add trace for stock prices
+    fig.add_trace(go.Candlestick(x=df['Date'],
+                open=df['OpeningPrice'],
+                high=df['High'],
+                low=df['Low'],
+                close=df['ClosingPrice'],
+                name=symbol))
+
+    # Set the title
+    fig.update_layout(title=f"Stock prices for {symbol}")
+
+    return fig
+
+def trade_view_graph(symbol):
+    html_code = f'''
+    <div id="tradingview_34f5a" class="tradingview-widget-container">
+        <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+        <script type="text/javascript">
+            var userLang = navigator.language || navigator.userLanguage;
+            var tradingViewLocale = userLang.substring(0, 2);
+            
+            new TradingView.widget({{
+                "width": 980,
+                "height": 610,
+                "symbol": "{symbol}",
+                "interval": "D",
+                "timezone": "Etc/UTC",
+                "theme": "dark",
+                "style": "1",
+                "locale": tradingViewLocale,
+                "toolbar_bg": "#f1f3f6",
+                "enable_publishing": false,
+                "allow_symbol_change": true,
+                "container_id": "tradingview_34f5a"
+            }});
+        </script>
+    </div>
+    '''
+    st.components.v1.html(html_code, width=1000, height=630)
 
 
 # Execute the main function
 if __name__ == '__main__':
 
-    # Set the page title and the layout
-    page_icon="🚀"  # You can specify the emoji as the icon
+    page_icon="🚀"  
     st.set_page_config(page_title="Predict Stock Prices", layout="wide", page_icon=page_icon)
     st.sidebar.title('Predict Stock Prices')
     streamlit_settings()
@@ -45,41 +100,51 @@ if __name__ == '__main__':
     # Display the selected stock name
     st.write(f"Stock Name: {symbol_to_name[selected_symbol]}")
 
-    # Fetch stock details for the selected symbol
-    selected_stock_details = dbm.fetch_stock_details_by_symbol(db, selected_symbol)
-
-    # Display stock details
-    if selected_stock_details:
-        st.sidebar.write("### Stock Details:")
-        columns = ['Symbol', 'Name', 'MarketCap', 'Country', 'IPOYear', 'Sector', 'Industry']
-        for col, value in zip(columns, selected_stock_details):
-            st.sidebar.write(f"**{col}:** {value}")
-    else:
-        st.sidebar.warning("No details found for the selected stock!")
-
-
-    # Fetch stock data for the selected symbol and convert it to a DataFrame
+        # Fetch stock data for the selected symbol and convert it to a DataFrame
     df = dbm.get_stock_data_as_dataframe(db, selected_symbol)
 
-    tb1, tb2, tb3,tb4,tb5 = st.tabs(["Data","Logistic Regression", "Random Forest & XGBoost", "Long Short-Term Memory (LSTM)", "Prophet"  ])
+    tb1, tb2, tb3,tb4,tb5, tb6 = st.tabs(["Info","Data","Logistic Regression", "Random Forest & XGBoost", "Long Short-Term Memory (LSTM)", "Prophet"  ])
 
+    
     with tb1:
+        
+        # fig = plot_stock_prices(selected_symbol)
+        # st.plotly_chart(fig)
+        trade_view_graph(selected_symbol)
+                
+        # Fetch stock details for the selected symbol
+        selected_stock_details = dbm.fetch_stock_details_by_symbol(db, selected_symbol)
+
+        # Display stock details
+        if selected_stock_details:
+            st.write("### Stock Details:")
+            columns = ['Symbol', 'Name', 'MarketCap', 'Country', 'IPOYear', 'Sector', 'Industry']
+            for col, value in zip(columns, selected_stock_details):
+                st.write(f"**{col}:** {value}")
+        else:
+            st.warning("No details found for the selected stock!")
+
+
+
+
+
+    with tb2:
         #Display the dataframe in Streamlit
         st.write(df)
     
-    with tb2:
+    with tb3:
         st.write("Logistic Regression")
 
-    with tb3:
+    with tb4:
         st.write("Random Forest & XGBoost")
     
-    with tb4:
+    with tb5:
         st.write("Long Short-Term Memory (LSTM)")
 
-    with tb5:
+    with tb6:
         st.write("Prophet")
     
-    
+
 
 
 
